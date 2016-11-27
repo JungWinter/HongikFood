e36b700913b6eaca29fcaa2010bbf39b82c099ac
winterJ <wintermy201@naver.com>
2016-11-27 23:57:42 +0900

    tagTostr함수에 _ 추가

--- app/request.py
+++ app/request.py
@@ -3,26 +3,31 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-def tagTostr(tag):
+soup = None
+
+def _tagTostr(tag):
     return tag.get_text().strip()
 
 def getSoup():
-    pass
-
-def updateDateAndMenu():
-    pass
-
-def updateAll():
     r = requests.get("http://apps.hongik.ac.kr/food/food.php")
     soup = BeautifulSoup(r.text, "lxml")
+    return soup
 
-    thead = tagTostr(soup.find("thead"))
+def returnDateAndMenu():
+    pass
+
+def adsf(soup=None):
+
+def initAll():
+    soup = getSoup()
+
+    thead = _tagTostr(soup.find("thead"))
     head = thead.split()[1:]
     key = [w[:3] for w in head]  # 월요일, 화요일, ...
     value = [w[4:-1] for w in head]  # 2016.11.21, 2016.11.22, ...
     dates = list(zip(wday, value))
 
-    subtitles = [tagTostr(i) for i in soup.find_all("tr", class_="subtitle")]
+    subtitles = [_tagTostr(i) for i in soup.find_all("tr", class_="subtitle")]
     '''
     subtitle은 학생회관 남문관은 장소 / 시간등의 정보
     그 외는 그냥 장소 로만 되어있음
@@ -33,7 +38,7 @@ def updateAll():
     Menu클래스에게 던져주면 알아서 판단하게끔
     '''
 
-    menus = [tagTostr(i) for i in soup.find_all("div", class_="daily-menu")]
+    menus = [_tagTostr(i) for i in soup.find_all("div", class_="daily-menu")]
     '''
     월 : 0, 6, 12 ..., 화 : 1, 7, 13 ...
     menus는 횡적으로 구성되어있음.
