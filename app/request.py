1d2c13fbb8fc15a2bc330cde896e46fdead510b9
winterJ <wintermy201@naver.com>
2016-11-28 12:22:18 +0900

    request 모듈 업데이트
    구현 끝
    lxml -> html.parser 변경
    getDatesAndMenus() 로 사용 가능

--- app/request.py
+++ app/request.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
 import requests
-import re
 from bs4 import BeautifulSoup
 
 soup = None
@@ -8,26 +6,39 @@ soup = None
 def _tagTostr(tag):
     return tag.get_text().strip()
 
-def getSoup():
+def _getSoup():
     r = requests.get("http://apps.hongik.ac.kr/food/food.php")
-    soup = BeautifulSoup(r.text, "lxml")
+    soup = BeautifulSoup(r.text, "html.parser")
     return soup
 
-def returnDateAndMenu():
-    pass
-
-def adsf(soup=None):
-
-def initAll():
-    soup = getSoup()
-
+def _soupToDates(soup):
     thead = _tagTostr(soup.find("thead"))
     head = thead.split()[1:]
     key = [w[:3] for w in head]  # 월요일, 화요일, ...
     value = [w[4:-1] for w in head]  # 2016.11.21, 2016.11.22, ...
-    dates = list(zip(wday, value))
+    dates = list(zip(key, value))
+    return dates
 
-    subtitles = [_tagTostr(i) for i in soup.find_all("tr", class_="subtitle")]
+def _soupToMenus(soup):
+    '''
+    월 : 0, 6, 12 ..., 화 : 1, 7, 13 ...
+    menus는 횡적으로 구성되어있음.
+    월요일에 대한 학생회관 점심, 저녁 / 남문관 점심 , 저녁 이렇게가 아니라
+    월화수목금토에 대한 학생회관 점심, 월화수목금토에 대한 학생회관 저녁 이렇게
+    총 54개 (하루에 9개 * 6일)
+    6개씩 건너뛰며 횡적 배열을 종적 배열로 바꿔줘야함
+    '''
+    menus = [_tagTostr(i).split() for i in soup.find_all("div", class_="daily-menu")]
+    for item in menus:
+        if item and item[0] in ["[중식]", "[석식]"]:
+            del item[0]
+    if len(menus) == 54:
+        transmenus = [list(menu) for menu in zip(*[iter(menus)]*6)]
+        rows = [iter(i) for i in transmenus]
+        cols = [list(col) for col in zip(*rows)]
+    return cols
+
+def _soupToSubtitles(soup):
     '''
     subtitle은 학생회관 남문관은 장소 / 시간등의 정보
     그 외는 그냥 장소 로만 되어있음
@@ -37,13 +48,16 @@ def initAll():
         교직원식당
     Menu클래스에게 던져주면 알아서 판단하게끔
     '''
+    subtitles = [_tagTostr(i) for i in soup.find_all("tr", class_="subtitle")]
+    return subtitles
 
-    menus = [_tagTostr(i) for i in soup.find_all("div", class_="daily-menu")]
+def getDatesAndMenus():
     '''
-    월 : 0, 6, 12 ..., 화 : 1, 7, 13 ...
-    menus는 횡적으로 구성되어있음.
-    월요일에 대한 학생회관 점심, 저녁 / 남문관 점심 , 저녁 이렇게가 아니라
-    월화수목금토에 대한 학생회관 점심, 월화수목금토에 대한 학생회관 저녁 이렇게
-    총 54개 (하루에 9개 * 6일)
-    6개씩 건너뛰며 횡적 배열을 종적 배열로 바꿔줘야함
+    dates와 menus를 반환함
+    menus는 2차원 리스트
+    dates는 튜플이 들어있는 리스트
     '''
+    soup = _getSoup()
+    dates = _soupToDates(soup)
+    menus = _soupToMenus(soup)
+    return dates, menus
