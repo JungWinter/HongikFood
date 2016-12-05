cd8d7c643ef1b19c044be36f8d237a0348d8fdbd
winterJ <wintermy201@naver.com>
2016-12-05 23:29:25 +0900

    Menu 테이블 추가, Poll 테이블 업데이트
    Poll테이블에서 Menu id를 받게끔 변경
    date필드를 모두 String으로 바꿈

--- app/models.py
+++ app/models.py
@@ -5,12 +5,14 @@ from datetime import datetime, timedelta
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_key = db.Column(db.String(32), index=True, unique=True)
-    join_date = db.Column(db.DateTime)
-    last_active_date = db.Column(db.DateTime)
+    join_date = db.Column(db.String())
+    last_active_date = db.Column(db.String())
 
     def __init__(self, user_key):
         self.user_key = user_key
-        self.join_date = datetime.utcnow() + timedelta(hours=9)
+        self.join_date = datetime.strftime(
+            datetime.utcnow() + timedelta(hours=9),
+            "%Y.%m.%d %H:%M:%S")
         self.last_active_date = self.join_date
 
     def __repr__(self):
@@ -19,22 +21,40 @@ class User(db.Model):
 
 class Poll(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    timestamp = db.Column(db.DateTime)
-    place = db.Column(db.String())
-    time = db.Column(db.String())
+    date = db.Column(db.String())
     score = db.Column(db.Integer)
+    menu_id = db.Column(db.Integer, db.ForeignKey("menu.id"))
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    menu = db.relationship("Menu", backref=db.backref("polls", lazy="dynamic"))
     user = db.relationship("User", backref=db.backref("polls", lazy="dynamic"))
 
-    def __init__(self, place, time, score, user):
-        self.place = place
-        self.time = time
+    def __init__(self, score, menu, user):
         self.score = score
+        self.menu = menu
         self.user = user
-        self.timestamp = datetime.utcnow() + timedelta(hours=9)
+        self.date = datetime.strftime(
+            datetime.utcnow() + timedelta(hours=9),
+            "%Y.%m.%d")
 
     def __repr__(self):
-        return "<Poll %r>" % (self.place+"-"+self.time+"-"+str(self.score))
+        return "<Poll %r>" % (self.user.user_key+"-"+str(self.score))
+
+
+class Menu(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String())
+    place = db.Column(db.String())
+    time = db.Column(db.String())
+    menu = db.Column(db.String())
+
+    def __init__(self, date, place, time, menu):
+        self.date = date
+        self.place = place
+        self.time = time
+        self.menu = menu
+
+    def __repr__(self):
+        return "<Menu %r>" % (self.date+"-"+self.place+"-"+self.time)
 
 
 class PlaceMenu():
